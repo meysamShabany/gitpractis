4ac46caf76abbb969d0c7b7469686b3cf65a0222
amirali8686 <143801316+amirali8686@users.noreply.github.com>
2025-05-07 21:53:59 +0330

    home module add

--- main.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import logging
-# file = open('file.txt', mode="a")
-# file.write(f"\ndate : {datetime.datetime.now()} ============= User with Create Success....")
-# file.close()
-
-with open("file.txt", mode='a') as file:
-    file.write(f"\ndate : {datetime.datetime.now()} ============= User with Create Success....")
